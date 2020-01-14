40705ebfb37777ec694a0faac4fcc0220d7a32f5
diegosilva91 <diegoarturosilva@hotmail.com>
2020-01-14 18:28:34 +0100

    add the control panel for admin

--- controllers/admin.php
+++ controllers/admin.php
@@ -14,35 +14,23 @@ class Admin extends MainController{
     }
     function render($param=null){
         // echo "$param"; var_dump($param);        
-        $this->view->posts=$this->models->get();
-        // var_dump($this->models);
-        // $this->loadModel('comment');
-        // $this->view->comments=$this->models->getComments();
-        
-        $this->loadModel('consult');
-        $params=$this->models->getPassByID($param);
-        $this->view->params=$params;
-        // $this->view->render('comments');
-        
-
-        // var_dump(!empty($_SESSION['user_id']));
-        if (isset( $_SESSION['user_id'] ) ) {
-            // echo "sesion ok";
-            // echo $_SESSION['user_id'];
-            // var_dump($_SESSION['user_id']);
-            $this->view->render('blog');
-            $this->view->session=$_SESSION['user_id'];
-            // header("location:".constant('URL').'blog?id='.$_SESSION['user_id']);
-            
-        } else {    
-        // Redirect them to the login page
-            $this->view->session=-1;
-            $this->view->render('blog');
-        }
-
-        // echo "render view";
-
+        $this->view->render('admin');  
     }
-
+    function posts($param=null){
+        // echo "$param"; var_dump($param);        
+        $this->view->render('admin');  
+    }
+    function users($param=null){
+        // echo "$param"; var_dump($param);        
+        $this->view->render('admin');  
+    }
+    function categories($param=null){
+        // echo "$param"; var_dump($param);        
+        $this->view->render('admin');  
+    }
+    function comments($param=null){
+        // echo "$param"; var_dump($param);        
+        $this->view->render('admin');  
+    }
 }
 ?>
